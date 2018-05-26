f55fe362f6ddd36db93288f662bd483701df2e9e
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-05-26 09:23:49 -0300

    Criação das entidades e enums

---
+++ src/main/java/com/michel/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,11 @@
+package com.michel.pontointeligente.api.enums;
+
+public enum TipoEnum {
+
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+}
