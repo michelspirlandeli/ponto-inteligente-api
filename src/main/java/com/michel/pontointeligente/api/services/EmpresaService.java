9307d71cafa12cb307356bed1cd4efecc1370f10
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-05-26 10:02:41 -0300

    Serviço de Empresa

---
+++ src/main/java/com/michel/pontointeligente/api/services/EmpresaService.java
@@ -0,0 +1,25 @@
+package com.michel.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import com.michel.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaService {
+
+	/**
+	 * Retorna uma empresa dado um CNPJ.
+	 * 
+	 * @param cnpj
+	 * @return Optional<Empresa>
+	 */
+	Optional<Empresa> buscarPorCnpj(String cnpj);
+	
+	/**
+	 * Cadastra uma nova empresa na base de dados.
+	 * 
+	 * @param empresa
+	 * @return Empresa
+	 */
+	Empresa persistir(Empresa empresa);
+	
+}
